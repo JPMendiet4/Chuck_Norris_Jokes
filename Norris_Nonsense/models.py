ebee67160b59ebacfeb536b9acb124146eed9e95
Juan P Mendieta C <jpmendietac@ut.edu.co>
2023-05-25 21:03:43 -0500

    CORS_2

--- Norris_Nonsense/models.py
+++ Norris_Nonsense/models.py
@@ -1,27 +1,9 @@
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 
 
-class CustomUser(BaseUserManager):
-    def _create_user(self, username, email, name, last_name, password, is_staff, is_superuser, **extra_fields):
-        user = self.model(
-            username=username,
-            email=email,
-            name=name,
-            last_name=last_name,
-            is_staff=is_staff,
-            is_superuser=is_superuser,
-            **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self.db)
-        return user
-
-    def create_user(self, username, email, name, last_name, password=None, **extra_fields):
-        return self._create_user(username, email, name, last_name, password, False, False, **extra_fields)
-
-    def create_superuser(self, username, email, name, last_name, password=None, **extra_fields):
-        return self._create_user(username, email, name, last_name, password, True, True, **extra_fields)
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=100, blank=True, null=True)
 
 
 class Joke(models.Model):
